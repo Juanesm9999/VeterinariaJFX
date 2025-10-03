7419cdf414629e4da755392931152514065f4880
Juanesm9999 <juane.mejiat@uqvirtual.edu.co>
2025-10-02 19:09:43 -0500

    Update

--- src/main/java/co/edu/uniquindio/poo/veterinariajfx/model/Mascota.java
+++ src/main/java/co/edu/uniquindio/poo/veterinariajfx/model/Mascota.java
@@ -1,48 +1,70 @@
 package co.edu.uniquindio.poo.veterinariajfx.model;
 
 public abstract class Mascota {
-    public class Mascota {
-        // Atributos Propios
-        protected String nombre;
-        protected String id;
-        protected String raza;
-        protected Double peso;
-        protected int EdadEnMeses;
+    // Atributos Propios
+    protected String nombre;
+    protected String id;
+    protected String raza;
+    protected Double peso;
+    protected int EdadEnMeses;
 
 
-        // Atributos de Relaciones
-        private Propietario thePropietario;
+    // Atributos de Relaciones
+    private Propietario thePropietario;
 
-        public Mascota(String nombre, String id) {
-            this.nombre = nombre;
-            this.id = id;
-        }
-
-        public String getNombre() {
-            return nombre;
-        }
-
-        public void setNombre(String nombre) {
-            this.nombre = nombre;
-        }
-
-        public String getId() {
-            return id;
-        }
-
-        public void setId(String id) {
-            this.id = id;
-        }
-
-        public Propietario getThePropietario() {
-            return thePropietario;
-        }
-
-        public void setThePropietario(Propietario thePropietario) {
-            this.thePropietario = thePropietario;
-        }
-
+    public Mascota(String nombre, String id) {
+        this.nombre = nombre;
+        this.id = id;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Propietario getThePropietario() {
+        return thePropietario;
+    }
+
+    public void setThePropietario(Propietario thePropietario) {
+        this.thePropietario = thePropietario;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    public int getEdadEnMeses() {
+        return EdadEnMeses;
+    }
+
+    public void setEdadEnMeses(int edadEnMeses) {
+        EdadEnMeses = edadEnMeses;
+    }
 }
+
+
+
--- src/main/java/co/edu/uniquindio/poo/veterinariajfx/model/Veterinaria.java
+++ src/main/java/co/edu/uniquindio/poo/veterinariajfx/model/Veterinaria.java
@@ -1,5 +1,6 @@
 package co.edu.uniquindio.poo.veterinariajfx.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Veterinaria {
@@ -11,8 +12,60 @@ public class Veterinaria {
     public Veterinaria(String nombre, String nit) {
         this.nombre = nombre;
         this.nit = nit;
+        listMascotas = new ArrayList<>();
+        listPropietarios = new ArrayList<>();
     }
 
+    // -------------------------------- CRUD Mascota ---------------------------------
+
+    public boolean agregarMascota(Mascota mascota) {
+        boolean centinela = false;
+        if (!verificarMascota(mascota.getId())) {
+            listMascotas.add(mascota);
+            centinela = true;
+        }
+        return centinela;
+    }
+
+    public boolean eliminarMascota(String id) {
+        boolean centinela = false;
+        for (Mascota mascota : listMascotas) {
+            if (mascota.getId().equals(id)) {
+                listMascotas.remove(mascota);
+                centinela = true;
+                break;
+            }
+        }
+        return centinela;
+    }
+
+
+    public boolean actualizarCliente(String id, Mascota actualizado) {
+        boolean centinela = false;
+        for (Mascota mascota : listMascotas) {
+            if (mascota.getId().equals(id)) {
+                mascota.setId(actualizado.getId());
+                mascota.setNombre(actualizado.getNombre());
+                mascota.setRaza(actualizado.getRaza());
+                centinela = true;
+                break;
+            }
+        }
+        return centinela;
+    }
+
+
+    public boolean verificarMascota(String id) {
+        boolean centinela = false;
+        for (Mascota mascota : listMascotas) {
+            if (mascota.getId().equals(id)) {
+                centinela = true;
+            }
+        }
+        return centinela;
+    }
+
+    // --------------------------------------------------------------------------------------------------------------
     public String getNit() {
         return nit;
     }
